b446bd6157795cb28a131495e6d026ec1d2dc566
4izer0 <ismael.dante89@gmail.com>
2022-04-30 14:31:17 -0500

    espada,llave

---
+++ Assets/PROGRAMACION/scripts/items.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class items : MonoBehaviour
+{
+    public int ID;
+    public string type;
+    public string descripcion;
+    public Sprite incon;
+
+    [HideInInspector]
+    public bool pickedUP;
+
+
+
+}
