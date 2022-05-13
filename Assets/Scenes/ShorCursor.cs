03503dbbe7928259d443fd115a52dcd5801ccf0b
zero <ismael.dante89@gmail.com>
2022-05-13 11:11:59 -0500

    XD

--- Assets/Scenes/ShorCursor.cs
+++ Assets/Scenes/ShorCursor.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class cursorr : MonoBehaviour
+public class ShorCursor : MonoBehaviour
 {
 
      void Start()
     {
         Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
     }
 
     
